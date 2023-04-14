075273433bcee2722dd9b605e714c503bdeab261
PancakesWithSyrup <33337475+PancakesWithSyrup@users.noreply.github.com>
2023-04-14 16:48:21 -0400

    Renamed main class file

--- AntiAdminDiscordBot.java
+++ AntiAdminDiscordBot.java
@@ -1,7 +1,7 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
-public class Watchdog {
+public class AntiAdminDiscordBot {
     public static void main(String[] args) throws Exception {
         JDA api = JDABuilder.createDefault("INSERT BOT KEY HERE")
                 .addEventListeners(new BotListener())
